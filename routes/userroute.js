49c0c9ff0480e37a86ef9068e88ce3d27c6ed20c
Emeamos <Emediongamos.engr@gmail.com>
2023-02-16 02:30:43 +0100

    updating user route

--- routes/userroute.js
+++ routes/userroute.js
@@ -3,7 +3,6 @@ import express from "express";
 import { deleteUserController, getUserController,userLoginController, updateUserController, userRegisterController } from "../controller/user/usercontroller.js";
 import User from "../model/usermodel.js";
 
-
 const userRoute = express.Router();
 
 userRoute.post("/register", userRegisterController)
