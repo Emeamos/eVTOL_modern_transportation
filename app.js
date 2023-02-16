e106c203a87cc3f0e72a0824ef4e61b2a4bdfb29
Emeamos <Emediongamos.engr@gmail.com>
2023-02-16 05:30:29 +0100

    updating app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import dbConnect from "./config/dbConnect.js";
+import userRoute from "./routes/userroute.js";
 
 dotenv.config()
 dbConnect()
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(express.json());
 
+app.use("/api/v1/users", userRoute);
 
 const PORT = process.env.PORT || 5555
 app.listen(PORT,console.log(`Server is running at ${PORT}`))
