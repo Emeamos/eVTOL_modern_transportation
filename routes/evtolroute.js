0cf837dc69309a43e6c15637e395d7d56bd38ed1
Emeamos <Emediongamos.engr@gmail.com>
2023-02-16 03:31:37 +0100

    updating evtol route

---
+++ routes/evtolroute.js
@@ -0,0 +1,16 @@
+import express from "express";
+import Evtol from "../model/evtolmodel";
+
+const evtolRoute = express.Router();
+
+evtolRoute.post("/register", async(req, res) => {
+    const {serialNumber, model, weightLimit, batteryCapacity, state} = req.body;
+    try {
+        res.json({
+            status: "success",
+            data: "you've successfully register an evtol"
+        })
+    } catch (error) {
+        res.json(error.message)
+    }
+})
